9b44101fe4d9adb25f6316487b2da491d9273aa2
新島由唯奈 <yuina-niijima@greensys.co.jp>
2026-02-12 13:47:44 +0900

    Revert "V1コピペ、エラー回避のためメイン画面クラス追加"
    This reverts commit 49e30d23ed963d6cc0e066c662a710153d76a84d.

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,20 @@
 import 'package:flutter/material.dart';
-//スプラッシュ画面
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  // バインディングの初期化(定型文)
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  // スプラッシュ画面が自動で消えないようにする
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-// 土台
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-// 中身
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    _dismissSplashScreen(); // ここでスプラッシュを制御する関数を呼ぶ
-  }
-
-  // スプラッシュ関数をいつ消すかの関数
-  void _dismissSplashScreen() async {
-    // ここで「2秒間」待機
-    await Future.delayed(const Duration(seconds: 2));
-
-    // 2秒経ったらスプラッシュを消して、アプリ画面を表示する
-    FlutterNativeSplash.remove();
-  }
-
-  // 読みやすくするためにメイン画面を別クラスに分ける
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: MainHomePage());
-  }
-}
-
-// --- ここから下を追加 ---
-class MainHomePage extends StatelessWidget {
-  const MainHomePage({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
   }
 }
