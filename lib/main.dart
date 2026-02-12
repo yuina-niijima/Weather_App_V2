5b8cd09adf4d1e8a7dd2c0ac86202a53fe737b5b
新島由唯奈 <yuina-niijima@greensys.co.jp>
2026-02-12 16:45:39 +0900

    V1のコードをコピペ、メイン画面のファイル未作成で発生したエラー回避のため、メイン画面のクラスを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,57 @@
 import 'package:flutter/material.dart';
+//スプラッシュ画面
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  runApp(const MainApp());
+  // バインディングの初期化(定型文)
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // スプラッシュ画面が自動で消えないようにする
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+// 土台
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+// 中身
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _dismissSplashScreen(); // ここでスプラッシュを制御する関数を呼ぶ
+  }
+
+  // スプラッシュ関数をいつ消すかの関数
+  void _dismissSplashScreen() async {
+    // ここで「2秒間」待機
+    await Future.delayed(const Duration(seconds: 2));
+
+    // 2秒経ったらスプラッシュを消して、アプリ画面を表示する
+    FlutterNativeSplash.remove();
+  }
+
+  // 読みやすくするためにメイン画面を別クラスに分ける
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainHomePage());
+  }
+}
+
+class MainHomePage extends StatelessWidget {
+  const MainHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('お天気アプリ'),
+      ),
     );
   }
 }
