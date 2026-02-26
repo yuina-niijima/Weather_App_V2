8b4229c39b7095a861e251586fd5f1d383fbdec8
新島由唯奈 <yuina-niijima@greensys.co.jp>
2026-02-26 15:13:25 +0900

    weather_detail_view_model作成

---
+++ lib/weather_detail_view_model.dart
@@ -0,0 +1,18 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:weather_app_v2/weather_data.dart';
+import 'package:weather_app_v2/weather_repository.dart';
+
+part 'weather_detail_view_model.g.dart';
+
+@riverpod
+final class WeatherDetailViewModel extends _$WeatherDetailViewModel {
+  @override
+  FutureOr<WeatherData> build(String city) async {
+    return _fetchWeather(city);
+  }
+
+  Future<WeatherData> _fetchWeather(String city) async {
+    final repository = ref.watch(weatherRepositoryProvider);
+    return repository.fetchWeather(city);
+  }
+}
