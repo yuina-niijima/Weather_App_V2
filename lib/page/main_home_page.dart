685c123c9db4d953dc395910e621f2d01793ee6f
新島由唯奈 <yuina-niijima@greensys.co.jp>
2026-03-02 20:49:00 +0900

    位置情報ダイアログの表示

--- lib/page/main_home_page.dart
+++ lib/page/main_home_page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:weather_app_v2/page/prefecture_screen.dart';
 
-class MainHomePage extends StatelessWidget {
+class MainHomePage extends HookConsumerWidget {
   const MainHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      Permission.locationWhenInUse.request();
+    }, const []);
     return Scaffold(
       body: SizedBox(
         width: double.infinity,
