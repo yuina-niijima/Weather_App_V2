70e5510f271db7f5de0344588583f7fc2f178cd1
新島由唯奈 <yuina-niijima@greensys.co.jp>
2026-03-06 18:12:47 +0900

    緯度経度を都市名に変換するリポジトリ追加

--- lib/repository/location_repository.dart
+++ lib/repository/location_repository.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:geocoding/geocoding.dart' as geo;
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -23,11 +25,22 @@ class LocationRepository {
 
   Future<LocationData> getCurrentLocationData() async {
     final pos = await getCurrentPosition();
-    return LocationData(
-      latitude: pos.latitude,
-      longitude: pos.longitude,
-      cityName: '現在地',
-    );
+
+    try {
+      await geo.setLocaleIdentifier("ja_JP");
+      List<geo.Placemark> placemarks = await geo.placemarkFromCoordinates(
+        pos.latitude,
+        pos.longitude,
+      );
+      if (placemarks.isNotEmpty) {
+        final place = placemarks.first;
+        final name = place.locality ?? place.administrativeArea ?? '現在地';
+        return City(name: name);
+      }
+    } catch (e) {
+      debugPrint('ジオコーディング失敗: $e');
+    }
+    return const City(name: '現在地');
   }
 }
 
