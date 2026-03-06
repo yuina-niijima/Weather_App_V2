5230c9759093631f5fe8ca40182d7bbb420bd4f9
新島由唯奈 <yuina-niijima@greensys.co.jp>
2026-03-06 18:01:55 +0900

    sealed classに定義し、geoNameメソッド追加

--- lib/model/location_data.dart
+++ lib/model/location_data.dart
@@ -1,11 +1,28 @@
-class LocationData {
-  final double latitude;
-  final double longitude;
-  final String? cityName;
+sealed class LocationData {
+  const LocationData();
+  String getName() {
+    final data = this;
+    if (data is City) {
+      return data.name;
+    }
+    return '現在地';
+  }
+}
 
-  LocationData({
-    required this.latitude,
-    required this.longitude,
-    this.cityName,
+class GeoCordinate extends LocationData {
+  final double lat;
+  final double lon;
+
+  const GeoCordinate({
+    required this.lat,
+    required this.lon,
+  });
+}
+
+class City extends LocationData {
+  final String name;
+
+  const City({
+    required this.name,
   });
 }
